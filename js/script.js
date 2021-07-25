f2fffc9b128ad981ef0af00fade1dd356ee41bf5
Pedro Hotz <pedrohotz2002@gmail.com>
2021-07-25 10:29:31 -0300

    Salvamento dos Pratos e Preço total adicionado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
+let botaoCompra = 0;
+let refeicao;
+let bebida;
+let sobremesa;
+let valorTotal = 0;
+let valorPrato = 0;
+let valorSobremesa = 0;
+let valorBebida = 0;
 function selecionar(elemento){
-    const selecionado = document.querySelector(".selecionado");
-
+    const selecionado = document.querySelector(".comida .selecionado");
     if(selecionado !== null){
-        selecionado.classList.remove("selecionado")
+        selecionado.classList.remove("selecionado");
+        botaoCompra--;
     }
     elemento.classList.add("selecionado");
+    if(elemento.classList.contains("selecionado")){
+        botaoCompra++;
+    }
+    let prato = elemento.querySelector(".comida .titulo-prato");
+    refeicao = prato.innerHTML;
+    let preco = elemento.querySelector(".comida .preco");
+    let valor = preco.innerHTML;
+    valorPrato = parseFloat(valor);
+    habilitarCompra();
 }
 
 
@@ -12,8 +29,18 @@ function selecionarBebida(elemento){
     const selecionado = document.querySelector(".bebida .selecionado");
     if(selecionado !== null){
         selecionado.classList.remove("selecionado")
+        botaoCompra--;
     }
     elemento.classList.add("selecionado");
+    if(elemento.classList.contains("selecionado")){
+        botaoCompra++;
+    }
+    let prato = elemento.querySelector(".bebida .titulo-prato");
+    let preco = elemento.querySelector(".bebida .preco");
+    let valor = preco.innerHTML;
+    valorBebida = parseFloat(valor);
+    bebida = prato.innerHTML;
+    habilitarCompra();
 }
 
 
@@ -21,6 +48,25 @@ function selecionarSobremesa(elemento){
     const selecionado = document.querySelector(".sobremesa .selecionado");
     if(selecionado !== null){
         selecionado.classList.remove("selecionado")
+        botaoCompra--;
     }
     elemento.classList.add("selecionado");
+    if(elemento.classList.contains("selecionado")){
+        botaoCompra++;
+    }
+    let prato = elemento.querySelector(".sobremesa .titulo-prato");
+    let preco = elemento.querySelector(".sobremesa .preco");
+    let valor = preco.innerHTML;
+    valorSobremesa = parseFloat(valor);
+    sobremesa = prato.innerHTML;
+    habilitarCompra();
+}
+
+function habilitarCompra(){
+    if (botaoCompra === 3){
+        valorTotal = valorPrato + valorBebida + valorSobremesa;
+        const botao = document.querySelector(".selet");
+        botao.innerHTML = "Fechar Pedido";
+        botao.classList.add("habilitado");
+    }
 }
